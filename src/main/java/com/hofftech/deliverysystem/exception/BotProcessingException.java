2fb20b05c3ebc674b35b783f2b4d54c46d02c18e
chernyshewww <nanodota69@gmail.com>
2025-01-15 20:23:43 +0300

    fixes after review

--- src/main/java/com/hofftech/deliverysystem/exception/BotProcessingException.java
+++ src/main/java/com/hofftech/deliverysystem/exception/BotProcessingException.java
@@ -1,11 +1,8 @@
 package com.hofftech.deliverysystem.exception;
 
 public class BotProcessingException extends RuntimeException {
+
     public BotProcessingException(String message) {
         super(message);
-    }
-
-    public BotProcessingException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
